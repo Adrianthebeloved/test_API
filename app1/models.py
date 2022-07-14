574ecf8a02343747b2dcbf41d0df22f35bbe80bd
Anthony Oliko <tobeoliko@gmail.com>
2022-07-14 15:25:04 +0100

    API updated

---
+++ app1/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=250)
+    salary = models.FloatField()
+    linkedin = models.URLField()
+    supervisor = models.ForeignKey(User, on_delete=models.CASCADE,
+    related_name='sub_ordinates')
+    department = models.CharField(max_length=350)
+    employee_num = models.CharField(max_length=6, null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# class Employer(models.Model):
+#     name = models.CharField(max_length=250)
+#     salary = models.FloatField()
+#     linkedin = models.URLField()
+#     manager = models.ForeignKey(User, on_delete=models.CASCADE,
+#     related_name='employee')
+#     Company = models.CharField(max_length=350)
+#     company_num = models.CharField(max_length=6, null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
